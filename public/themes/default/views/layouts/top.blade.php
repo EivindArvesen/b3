f40b5a9d91d3ddb61aac770a2bffc94d4587601c
Eivind Arvesen <eivind.arvesen@gmail.com>
2016-09-11 14:38:24 +0200

    Add minor changes to navbar; Change layout container style; Clean up in theme asset dist

--- public/themes/default/views/layouts/top.blade.php
+++ public/themes/default/views/layouts/top.blade.php
@@ -12,7 +12,7 @@
     <title>{{config('bbb_config.site_name')}} - {{$page_title}}</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="/themes/{{config('bbb_config.theme')}}/assets/dist/main.min.638e6a110f7e5eb1.css" rel="stylesheet">
+    <link href="/themes/{{config('bbb_config.theme')}}/assets/dist/main.min.efa675b13b9e62df.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
