761d6171c3028f57b5b14cf8cad5afe4b4a4a595
Eivind Arvesen <eivind.arvesen@gmail.com>
2016-10-10 00:18:09 +0200

    Update default config file

--- config/b3_config.php
+++ config/b3_config.php
@@ -5,17 +5,17 @@ return [
     'site_name' => 'Site Name',
     'user' => 'John Doe',
     'email' => 'email@provider.com',
-    'theme' => 'eivindarvesen',
+    'theme' => 'default',
     'status' => 'live', // live, maintenance, unavailable, landing, prelaunch
     'debug' => True, // echo debug info on failure
     'backend' => 'db',
     'cache-age' => 24, // max cache age in hours
-    // Blog
-    'about' => "Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.",
+    // Blog sidebar
+    'about' => "",
     // Meta description
-    'description' => 'A page about...',
+    'description' => "",
     // Social media accounts (usernames for links)
-    'github' => 'eivind88',
-    'twitter' => 'Eivind88',
-    'facebook' => 'arvesen'
+    'github' => '',
+    'twitter' => '',
+    'facebook' => ''
 ];
