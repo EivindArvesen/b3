74dd22b2fa3ece8911d5546dba7df97c734abdf0
Eivind Arvesen <eivind.arvesen@gmail.com>
2016-10-12 22:46:05 +0200

    Make minor updates to blog post style

--- public/themes/default/views/layouts/top.blade.php
+++ public/themes/default/views/layouts/top.blade.php
@@ -16,7 +16,7 @@
     <title>{{config('b3_config.site_name')}} - {{$page_title}}</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="/themes/{{config('b3_config.theme')}}/assets/dist/main.min.0728d6ca6691849b.css" rel="stylesheet">
+    <link href="/themes/{{config('b3_config.theme')}}/assets/dist/main.min.4e7cc9957e308c33.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
