befa155e3d88d1dccf7e33a989bdc1a77a93710e
Eivind Arvesen <eivind.arvesen@gmail.com>
2016-10-27 22:41:45 +0200

    Fix menu styles

--- public/themes/default/views/layouts/top.blade.php
+++ public/themes/default/views/layouts/top.blade.php
@@ -15,7 +15,7 @@
     <title>{{config('b3_config.site-name')}} - {{$page_title}}</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="/themes/{{config('b3_config.theme')}}/assets/dist/main.min.a5c00c16a8b157e2.css" rel="stylesheet">
+    <link href="/themes/{{config('b3_config.theme')}}/assets/dist/main.min.7f505c49eaa4327a.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
