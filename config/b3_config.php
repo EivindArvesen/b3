b55a4ef6d03ec3b19f521707cf69de44f1d196a1
Eivind Arvesen <eivind.arvesen@gmail.com>
2016-11-03 20:52:50 +0100

    Revert to example config file

--- config/b3_config.php
+++ config/b3_config.php
@@ -2,22 +2,21 @@
 
 return [
     // Basic author information
-    'site-name' => 'Eivind Arvesen',
-    'user' => 'Eivind Arvesen',
-    'email' => 'eivind.arvesen@gmail.com',
+    'site-name' => 'Site Name',
+    'user' => 'John Doe',
+    'email' => 'email@provider.com',
     'theme' => 'default',
-    'status' => 'live', // live, maintenance, unavailable, landing, prelaunch
+    'status' => 'prelaunch', // live, maintenance, unavailable, landing, prelaunch
     'debug' => False, // echo DEBUG pages + debug info on failure
-    'date-format' => 'DMY', // some combination of DMY
+    'date-format' => 'YMD', // some combination of DMY
     'backend' => 'db',
     'cache-age' => 24, // max cache age in hours
     // Blog sidebar
-    'about' => "This is where I'll write about anything that interests me.",
-    // Site metadata
+    'about' => "This appears in the blog sidebar.",
+    // Meta description
     'description' => "A site built using B3",
-    'keywords' => ['website', 'blog'],
     // Social media accounts (usernames for links)
-    'github' => 'eivind88',
-    'twitter' => 'Eivind88',
-    'facebook' => 'arvesen'
+    'github' => '',
+    'twitter' => '',
+    'facebook' => ''
 ];
