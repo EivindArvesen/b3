78a839a3c7c05df4641cc2d4a1e99b1dd85d57d6
Eivind Arvesen <eivind.arvesen@gmail.com>
2016-11-27 01:02:50 +0100

    Add hack in db population to make it work in production

--- scripts/populate-db.sh
+++ scripts/populate-db.sh
@@ -3,5 +3,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/..
 pwd
-php $(dirname $DIR)/artisan migrate:refresh && php $(dirname $DIR)/composer.phar dump-autoload && php $(dirname $DIR)/artisan db:seed -vvv
+php $(dirname $DIR)/artisan migrate:refresh --force && php $(dirname $DIR)/composer.phar dump-autoload && php $(dirname $DIR)/artisan db:seed -vvv --force
 cd - > /dev/null 2>&1
