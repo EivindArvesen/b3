c5157c347a4a3202de10cdc38cd9f49bd440c824
Eivind Arvesen <eivind.arvesen@gmail.com>
2017-01-26 18:35:28 +0100

    Fix search result and list templates

--- public/themes/default/views/blog/list.blade.php
+++ public/themes/default/views/blog/list.blade.php
@@ -2,14 +2,19 @@
 
 @section('content')
 
-        <div class="col-sm-8 blog-main">
-
-            <ol class="breadcrumb">
+        <section class="col-sm-12 blog-top">
+            <ol class="breadcrumb col-sm-8">
               <li><a href="/blog">Blog</a></li>
               @if ($list_title!='')
                 <li><a href="/blog/{{strtolower($list_title)}}">{{ucfirst(strtolower($list_title))}}</a></li>
             @endif
             </ol>
+            <div class="col-sm-3 col-sm-offset-1 searchbar">
+                @include('blog.searchbar')
+            </div>
+        </section>
+
+        <section class="col-sm-8 blog-main blog-inventory">
 
             <ul class="list-unstyled inventory">
                 @if ($list_title=='Language')
@@ -39,7 +44,7 @@
                 @endif
             </ul>
 
-        </div><!-- /.blog-main -->
+        </section><!-- /.blog-main -->
         @include('blog.sidebar')
 
         @include('layouts.pagination')
--- public/themes/default/views/blog/search.blade.php
+++ public/themes/default/views/blog/search.blade.php
@@ -2,13 +2,17 @@
 
 @section('content')
 
-        <section class="col-sm-8 blog-main blog-inventory">
-
-            <ol class="breadcrumb">
+        <section class="col-sm-12 blog-top">
+            <ol class="breadcrumb col-sm-8">
                 <li><a href="/blog">Blog</a></li>
                 <li><a href="/blog/search/">Search</a></li>
                 <li class="active">{{$query}}</li>
             </ol>
+            <div class="col-sm-3 col-sm-offset-1 searchbar">
+                @include('blog.searchbar')
+            </div>
+
+        <section class="col-sm-8 blog-main blog-inventory">
 
             @foreach ($results as $result)
                 @include('blog.item')
