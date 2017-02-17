c77927637c3fdbba2c172ec6766448e0efab5cd2
Eivind Arvesen <eivind.arvesen@gmail.com>
2017-02-17 19:03:28 +0100

    Style modals called from jumbotrons

--- public/themes/default/views/layouts/top.blade.php
+++ public/themes/default/views/layouts/top.blade.php
@@ -25,7 +25,7 @@
     @endif
 
     <!-- Theme CSS -->
-    <link href="/themes/{{config('b3_config.theme')}}/assets/dist/styles/style.min.eb1236308cc9f68b.css" rel="stylesheet">
+    <link href="/themes/{{config('b3_config.theme')}}/assets/dist/styles/style.min.87f16edf6503a1bb.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
