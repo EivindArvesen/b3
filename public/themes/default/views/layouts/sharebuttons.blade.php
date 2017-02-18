8416e6d01789f83775e33f38dadb854ac050d710
Eivind Arvesen <eivind.arvesen@gmail.com>
2017-02-18 17:10:07 +0100

    Fix typo in sharebuttons

--- public/themes/default/views/layouts/sharebuttons.blade.php
+++ public/themes/default/views/layouts/sharebuttons.blade.php
@@ -5,5 +5,5 @@
     <a class="btn btn-default btn-ghost btn-primary btn-xs" href="https://plus.google.com/share?url={{Request::url()}}">Google Plus</a>
     <a class="btn btn-default btn-ghost btn-primary btn-xs" href="http://pinterest.com/pin/create/button/?url={{Request::url()}}">Pinterest</a>
     <a class="btn btn-default btn-ghost btn-primary btn-xs" href="https://www.linkedin.com/shareArticle?mini=true&amp;url={{Request::url()}}">LinkedIn</a>
-    <a class="btn btn-default btn-ghost btn-primary btn-xs" href="mailto:?subject=Check this page out&amp;body=Hey,%20check%20this%20:{{Request::url()}}">Mail</a>
+    <a class="btn btn-default btn-ghost btn-primary btn-xs" href="mailto:?subject=Check this page out&amp;body=Hey,%20check%20this%20:{{Request::url()}}">Email</a>
 </div>
