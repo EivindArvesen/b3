de2b698d938e31583a1842745e9ad5daa52cf92f
Eivind Arvesen <eivind.arvesen@gmail.com>
2017-02-19 02:09:26 +0100

    Fix styling issue with share buttons

--- public/themes/default/views/layouts/sharebuttons.blade.php
+++ public/themes/default/views/layouts/sharebuttons.blade.php
@@ -1,9 +1,11 @@
 <div class="share-buttons text-center">
     <p><strong>Share this post</strong></p>
-    <a class="btn btn-default btn-ghost btn-primary btn-xs" href="https://www.facebook.com/sharer/sharer.php?u={{Request::url()}}">Facebook</a>
-    <a class="btn btn-default btn-ghost btn-primary btn-xs" href="https://twitter.com/intent/tweet?text={{Request::url()}}">Twitter</a>
-    <a class="btn btn-default btn-ghost btn-primary btn-xs" href="https://plus.google.com/share?url={{Request::url()}}">Google Plus</a>
-    <a class="btn btn-default btn-ghost btn-primary btn-xs" href="http://pinterest.com/pin/create/button/?url={{Request::url()}}">Pinterest</a>
-    <a class="btn btn-default btn-ghost btn-primary btn-xs" href="https://www.linkedin.com/shareArticle?mini=true&amp;url={{Request::url()}}">LinkedIn</a>
-    <a class="btn btn-default btn-ghost btn-primary btn-xs" href="mailto:?subject=Check this page out&amp;body=Hey,%20check%20this%20:{{Request::url()}}">Email</a>
+    <div class="share-container">
+        <a class="btn btn-default btn-ghost btn-primary btn-xs" href="https://www.facebook.com/sharer/sharer.php?u={{Request::url()}}">Facebook</a>
+        <a class="btn btn-default btn-ghost btn-primary btn-xs" href="https://twitter.com/intent/tweet?text={{Request::url()}}">Twitter</a>
+        <a class="btn btn-default btn-ghost btn-primary btn-xs" href="https://plus.google.com/share?url={{Request::url()}}">Google Plus</a>
+        <a class="btn btn-default btn-ghost btn-primary btn-xs" href="http://pinterest.com/pin/create/button/?url={{Request::url()}}">Pinterest</a>
+        <a class="btn btn-default btn-ghost btn-primary btn-xs" href="https://www.linkedin.com/shareArticle?mini=true&amp;url={{Request::url()}}">LinkedIn</a>
+        <a class="btn btn-default btn-ghost btn-primary btn-xs" href="mailto:?subject=Check this page out&amp;body=Hey,%20check%20this%20:{{Request::url()}}">Email</a>
+    </div>
 </div>
--- public/themes/default/views/layouts/top.blade.php
+++ public/themes/default/views/layouts/top.blade.php
@@ -25,7 +25,7 @@
     @endif
 
     <!-- Theme CSS -->
-    <link href="/themes/{{config('b3_config.theme')}}/assets/dist/styles/style.min.b87f439d9b97efd4.css" rel="stylesheet">
+    <link href="/themes/{{config('b3_config.theme')}}/assets/dist/styles/style.min.4fef25c7676fc400.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
