e9cc3a4eccb6d9c3212992705dbd50aadec5a8a5
Eivind Arvesen <eivind.arvesen@gmail.com>
2017-03-18 02:31:27 +0100

    Center youtube thumbnails in lazy load placeholders vertically

--- public/themes/default/views/layouts/top.blade.php
+++ public/themes/default/views/layouts/top.blade.php
@@ -25,7 +25,7 @@
     @endif
 
     <!-- Theme CSS -->
-    <link href="/themes/{{config('b3_config.theme')}}/assets/dist/styles/style.min.c4998f0aef220795.css" rel="stylesheet">
+    <link href="/themes/{{config('b3_config.theme')}}/assets/dist/styles/style.min.2a99ef604b178bef.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
