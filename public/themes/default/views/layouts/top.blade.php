57ba743b5d53d04676eba08a7c90b70f521d8dda
Eivind Arvesen <eivind.arvesen@gmail.com>
2017-03-21 22:53:53 +0100

    Style changes

--- public/themes/default/views/layouts/top.blade.php
+++ public/themes/default/views/layouts/top.blade.php
@@ -25,7 +25,7 @@
     @endif
 
     <!-- Theme CSS -->
-    <link href="/themes/{{config('b3_config.theme')}}/assets/dist/styles/style.min.a816a20c72512574.css" rel="stylesheet">
+    <link href="/themes/{{config('b3_config.theme')}}/assets/dist/styles/style.min.a520da7777226c15.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
