c14666de1224807bf7f0d660938109418250a862
Eivind Arvesen <eivind.arvesen@gmail.com>
2017-05-29 20:10:00 +0200

    Make share buttons use new tab

--- public/themes/default/views/layouts/sharebuttons.blade.php
+++ public/themes/default/views/layouts/sharebuttons.blade.php
@@ -1,11 +1,11 @@
 <div class="share-buttons text-center">
     <p><strong>Share this post</strong></p>
     <div class="share-container">
-        <a class="btn btn-default btn-ghost btn-primary btn-xs" href="https://www.facebook.com/sharer/sharer.php?u={{Request::url()}}">Facebook</a>
-        <a class="btn btn-default btn-ghost btn-primary btn-xs" href="https://twitter.com/intent/tweet?text={{Request::url()}}">Twitter</a>
-        <a class="btn btn-default btn-ghost btn-primary btn-xs" href="https://plus.google.com/share?url={{Request::url()}}">Google Plus</a>
-        <a class="btn btn-default btn-ghost btn-primary btn-xs" href="http://pinterest.com/pin/create/button/?url={{Request::url()}}">Pinterest</a>
-        <a class="btn btn-default btn-ghost btn-primary btn-xs" href="https://www.linkedin.com/shareArticle?mini=true&amp;url={{Request::url()}}">LinkedIn</a>
-        <a class="btn btn-default btn-ghost btn-primary btn-xs" href="mailto:?subject=Check this page out&amp;body=Hey,%20check%20this%20:{{Request::url()}}">Email</a>
+        <a target="_blank" class="btn btn-default btn-ghost btn-primary btn-xs" href="https://www.facebook.com/sharer/sharer.php?u={{Request::url()}}">Facebook</a>
+        <a target="_blank" class="btn btn-default btn-ghost btn-primary btn-xs" href="https://twitter.com/intent/tweet?text={{Request::url()}}">Twitter</a>
+        <a target="_blank" class="btn btn-default btn-ghost btn-primary btn-xs" href="https://plus.google.com/share?url={{Request::url()}}">Google Plus</a>
+        <a target="_blank" class="btn btn-default btn-ghost btn-primary btn-xs" href="http://pinterest.com/pin/create/button/?url={{Request::url()}}">Pinterest</a>
+        <a target="_blank" class="btn btn-default btn-ghost btn-primary btn-xs" href="https://www.linkedin.com/shareArticle?mini=true&amp;url={{Request::url()}}">LinkedIn</a>
+        <a target="_blank" class="btn btn-default btn-ghost btn-primary btn-xs" href="mailto:?subject=Check this page out&amp;body=Hey,%20check%20this%20out: {{Request::url()}}">Email</a>
     </div>
 </div>
