cc093023ee6fdf6558232f5bc01030516b6ad745
Eivind Arvesen <eivind.arvesen@gmail.com>
2021-01-08 23:35:45 +0100

    Ensure closing of tags in post intros

--- config/b3_functions.php
+++ config/b3_functions.php
@@ -171,6 +171,26 @@ function read_time($id, $only_minutes = false, $short = false) {
   return $est;
 }
 
+function closetags($html) {
+    preg_match_all('#<(?!meta|img|br|hr|input\b)\b([a-z]+)(?: .*)?(?<![/|/ ])>#iU', $html, $result);
+    $openedtags = $result[1];
+    preg_match_all('#</([a-z]+)>#iU', $html, $result);
+    $closedtags = $result[1];
+    $len_opened = count($openedtags);
+    if (count($closedtags) == $len_opened) {
+        return $html;
+    }
+    $openedtags = array_reverse($openedtags);
+    for ($i=0; $i < $len_opened; $i++) {
+        if (!in_array($openedtags[$i], $closedtags)) {
+            $html .= '</'.$openedtags[$i].'>';
+        } else {
+            unset($closedtags[array_search($openedtags[$i], $closedtags)]);
+        }
+    }
+    return $html;
+}
+
 function get_intro($id) {
   $post_object = Blogpost::where('post_id', $id)->get(['cover', 'lead', 'body'])[0];
   $post = $post_object['body'];
@@ -208,7 +228,7 @@ function get_intro($id) {
   } else {
     $last_space = strrpos($string, ' ');
     $last_word = substr($string, $last_space);
-    $first_chunk = substr($string, 0, $last_space) . '...';
+    $first_chunk = closetags(substr($string, 0, $last_space). '...');
   }
 
   return implode($image[0]).$first_chunk;
